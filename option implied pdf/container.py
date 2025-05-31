7716e54ae5139a572e4b97c7d8db909762f60d5f
EttoreEder <ettoreeder@gmail.com>
2025-05-31 11:38:04 +0200

    importing options data to container

--- option implied pdf/container.py
+++ option implied pdf/container.py
@@ -4,5 +4,8 @@ import scipy.signal as sc
 import time
 
 class Container:
+    """
+    Container class to hold variables or functions needed globally in all classes. Can be extended to include more variables or functions.
+    """
     def __init__(self):
         self.fetchUrl = 'https://www.deribit.com/api/v2/public/get_book_summary_by_currency?currency=BTC&kind=option'
--- option implied pdf/importer.py
+++ option implied pdf/importer.py
@@ -1,7 +1,12 @@
 import numpy as np
 import requests
+from datetime import datetime
 
 class Importer:
+    """
+    Importer class to fetch option price data from the Deribit API and store it in the container.
+    This class is responsible for making the API call and processing the response to extract relevant option data.
+    """
     def __init__(self, container):
         self.container = container
     
@@ -9,8 +14,25 @@ class Importer:
         info_response = requests.get(self.container.fetchUrl)
         data = info_response.json()["result"]
 
-        self.container.optionsDataCall = {}
-        self.container.optionsDataPut = {}
+        self.container.optionsDataCall = {} # structure: {T1: [(strikePrice1, priceBTC1), (strikePrice2, priceBTC2), ...], T2: [(strikePrice1, priceBTC1), ...], ...}
+        self.container.optionsDataPut = {} # structure: same as optionsDataCall but for put options
         for option in data:
             T, strikePrice, p_or_c = option['instrument_name'].split('-')[1:]
-            
+            # Convert T (format: 25JUL25) to a date object
+            T_date = datetime.strptime(T, "%d%b%y").date()
+            # Convert T_date to a number of days from today
+            days_from_today = (T_date - datetime.today().date()).days
+            if option['mid_price'] is None:
+                continue
+            if p_or_c == "P":
+                if days_from_today not in self.container.optionsDataPut:
+                    self.container.optionsDataPut[days_from_today] = []
+                self.container.optionsDataPut[days_from_today].append((float(strikePrice), float(option['mid_price']) * float(option['underlying_price'])))
+            elif p_or_c == "C":
+                if days_from_today not in self.container.optionsDataCall:
+                    self.container.optionsDataCall[days_from_today] = []
+                self.container.optionsDataCall[days_from_today].append((float(strikePrice), float(option['mid_price']) * float(option['underlying_price'])))
+
+        # Sort the options data by expiration date
+        self.container.optionsDataCall = dict(sorted(self.container.optionsDataCall.items()))
+        self.container.optionsDataPut = dict(sorted(self.container.optionsDataPut.items()))
